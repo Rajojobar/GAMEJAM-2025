371b117e8cbdc08717e2a6a56d230928ef028c22
raphaël <r.bar772@laposte.net>
2025-11-01 15:17:52 +0100

    scène principale est calibrée correctement

---
+++ script/player.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("move_right"):
+		sprite.play("marche")
+#		position.x += 100 * delta  # vitesse simple
+		sprite.flip_h = false      # orientation droite
+	else:
+		sprite.play("default")
